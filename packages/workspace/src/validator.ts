5e1448cf869230800e2d7ec18f78b4c180e3214d
dantal4 <70883080+dantal4@users.noreply.github.com>
2025-04-22 22:32:59 +0300

    SALTO-7069 another improvement in validateElement time performance (#160)

--- packages/workspace/src/validator.ts
+++ packages/workspace/src/validator.ts
@@ -61,6 +61,12 @@ const { makeArray } = collections.array
 
 const MAX_VALUE_LENGTH = 25
 
+type ValidationContext = {
+  validatedReferences: Set<string>
+  requiredFieldsByType: Map<string, Field[]>
+  restrictedFieldsByType: Map<string, Field[]>
+}
+
 export abstract class ValidationError
   extends types.Bean<{
     elemID: ElemID
@@ -543,12 +549,29 @@ const validatePrimitiveValueType = (elemID: ElemID, value: Value, type: Primitiv
 
 const isEmptyArray = (value: unknown): boolean => Array.isArray(value) && value.length === 0
 
-const validateRequiredFields = (elemID: ElemID, value: Value, type: TypeElement): ValidationError[] => {
+const getRequiredFields = (type: ObjectType, context: ValidationContext): Field[] => {
+  const typeId = type.elemID.getFullName()
+  const requiredFields = context.requiredFieldsByType.get(typeId)
+  if (requiredFields !== undefined) {
+    return requiredFields
+  }
+  const calculatedRequiredFields = Object.values(type.fields).filter(
+    field => field.annotations[CORE_ANNOTATIONS.REQUIRED] === true,
+  )
+  context.requiredFieldsByType.set(typeId, calculatedRequiredFields)
+  return calculatedRequiredFields
+}
+
+const validateRequiredFields = (
+  elemID: ElemID,
+  value: Value,
+  type: TypeElement,
+  context: ValidationContext,
+): ValidationError[] => {
   if (!isObjectType(type) || elemID.idType !== 'instance') {
     return []
   }
-  return Object.values(type.fields)
-    .filter(field => field.annotations[CORE_ANNOTATIONS.REQUIRED] === true)
+  return getRequiredFields(type, context)
     .filter(
       field => value[field.name] === undefined || (isEmptyArray(value[field.name]) && !isListType(field.refType.type)),
     )
@@ -561,11 +584,27 @@ const validateRequiredFields = (elemID: ElemID, value: Value, type: TypeElement)
     )
 }
 
-const validateRestrictedFields = (elemID: ElemID, value: Value, type: TypeElement): ValidationError[] => {
+const getRestrictedFields = (type: ObjectType, context: ValidationContext): Field[] => {
+  const typeId = type.elemID.getFullName()
+  const restrictedFields = context.restrictedFieldsByType.get(typeId)
+  if (restrictedFields !== undefined) {
+    return restrictedFields
+  }
+  const calculatedRestrictedFields = Object.values(type.fields).filter(field => !_.isEmpty(getRestriction(field)))
+  context.restrictedFieldsByType.set(typeId, calculatedRestrictedFields)
+  return calculatedRestrictedFields
+}
+
+const validateRestrictedFields = (
+  elemID: ElemID,
+  value: Value,
+  type: TypeElement,
+  context: ValidationContext,
+): ValidationError[] => {
   if (!isObjectType(type) || elemID.idType !== 'instance') {
     return []
   }
-  return Object.values(type.fields)
+  return getRestrictedFields(type, context)
     .filter(field => value[field.name] !== undefined)
     .flatMap(field => validateRestrictionsValue(elemID.createNestedID(field.name), value[field.name], field))
 }
@@ -574,12 +613,13 @@ const validateReferenceExpression = (
   elemID: ElemID,
   value: ReferenceExpression,
   type: TypeElement,
-  validatedReferences: Set<string>,
+  context: ValidationContext,
 ): ValidationError[] => {
+  const { validatedReferences } = context
   if (!isElement(value.value) && !validatedReferences.has(value.elemID.getFullName())) {
     validatedReferences.add(value.elemID.getFullName())
     // eslint-disable-next-line no-use-before-define
-    const result = validateValue(elemID, value.value, type, validatedReferences)
+    const result = validateValue(elemID, value.value, type, context)
     validatedReferences.delete(value.elemID.getFullName())
     return result
   }
@@ -590,24 +630,24 @@ const validateTemplateExpression = (
   elemID: ElemID,
   value: TemplateExpression,
   type: TypeElement,
-  validatedReferences: Set<string>,
+  context: ValidationContext,
 ): ValidationError[] => {
-  const templatedReferenceValidationErrors = value.parts
-    .map(part => (isReferenceExpression(part) ? createReferenceValidationErrors(elemID, part.value) : []))
-    .flat()
+  const templatedReferenceValidationErrors = value.parts.flatMap(part =>
+    isReferenceExpression(part) ? createReferenceValidationErrors(elemID, part.value) : [],
+  )
   // eslint-disable-next-line no-use-before-define
-  return templatedReferenceValidationErrors.concat(validateValue(elemID, value.value, type, validatedReferences))
+  return templatedReferenceValidationErrors.concat(validateValue(elemID, value.value, type, context))
 }
 
 const validateObjectValue = (
   elemID: ElemID,
   value: Value,
   type: ObjectType | MapType,
-  validatedReferences: Set<string>,
+  context: ValidationContext,
 ): ValidationError[] => {
   if (!_.isObjectLike(value)) {
     // TODO: we shouldn't validate required fields for non-object values
-    return validateRequiredFields(elemID, value, type).concat(
+    return validateRequiredFields(elemID, value, type, context).concat(
       new InvalidValueTypeValidationError({
         elemID,
         value,
@@ -616,9 +656,9 @@ const validateObjectValue = (
     )
   }
 
-  const missingRequiredFieldsErrors = validateRequiredFields(elemID, value, type)
+  const missingRequiredFieldsErrors = validateRequiredFields(elemID, value, type, context)
   const additionalPropertiesErrors = validateNoAdditionalProperties(elemID, value, type)
-  const restrictedFieldsErrors = validateRestrictedFields(elemID, value, type)
+  const restrictedFieldsErrors = validateRestrictedFields(elemID, value, type, context)
 
   const fieldValidationErrors = Object.keys(value).flatMap(k =>
     // eslint-disable-next-line no-use-before-define
@@ -626,7 +666,7 @@ const validateObjectValue = (
       elemID.createNestedID(k),
       value[k],
       (isObjectType(type) ? type.fields[k]?.refType.type : type.refInnerType.type) ?? BuiltinTypes.UNKNOWN,
-      validatedReferences,
+      context,
     ),
   )
 
@@ -636,21 +676,30 @@ const validateObjectValue = (
     .concat(restrictedFieldsErrors)
 }
 
-const validateUnknownValue = (elemID: ElemID, value: Value, validatedReferences: Set<string>): ValidationError[] => {
+const validateUnknownValue = (elemID: ElemID, value: Value, context: ValidationContext): ValidationError[] => {
   if (!_.isObjectLike(value)) {
     return []
   }
   return Object.keys(value).flatMap(k =>
     // eslint-disable-next-line no-use-before-define
-    validateValue(elemID.createNestedID(k), value[k], BuiltinTypes.UNKNOWN, validatedReferences),
+    validateValue(elemID.createNestedID(k), value[k], BuiltinTypes.UNKNOWN, context),
   )
 }
+
+const validateListItems = (
+  elemID: ElemID,
+  value: Value[],
+  innerType: TypeElement,
+  context: ValidationContext,
+): ValidationError[] =>
+  // eslint-disable-next-line no-use-before-define
+  value.flatMap((val, i) => validateValue(elemID.createNestedID(String(i)), val, innerType, context))
 
 const validateListValue = (
   elemID: ElemID,
   value: Value,
   type: ListType,
-  validatedReferences: Set<string>,
+  context: ValidationContext,
 ): ValidationError[] => {
   const innerType = type.refInnerType.type
   if (!isType(innerType)) {
@@ -664,10 +713,9 @@ const validateListValue = (
     return []
   }
   return Array.isArray(value)
-    ? // eslint-disable-next-line no-use-before-define
-      value.flatMap((val, i) => validateValue(elemID.createNestedID(String(i)), val, innerType, validatedReferences))
+    ? validateListItems(elemID, value, innerType, context)
     : // eslint-disable-next-line no-use-before-define
-      validateValue(elemID, value, innerType, validatedReferences)
+      validateValue(elemID, value, innerType, context)
 }
 
 const validatePrimitiveValue = (elemID: ElemID, value: Value, type: PrimitiveType): ValidationError[] => {
@@ -681,18 +729,18 @@ const validateValue = (
   elemID: ElemID,
   value: Value,
   type: TypeElement,
-  validatedReferences = new Set<string>(),
+  context: ValidationContext,
 ): ValidationError[] => {
   if (Array.isArray(value) && !isListType(type)) {
-    return validateValue(elemID, value, new ListType(type), validatedReferences)
+    return validateListItems(elemID, value, type, context)
   }
 
   if (isReferenceExpression(value)) {
-    return validateReferenceExpression(elemID, value, type, validatedReferences)
+    return validateReferenceExpression(elemID, value, type, context)
   }
 
   if (isTemplateExpression(value)) {
-    return validateTemplateExpression(elemID, value, type, validatedReferences)
+    return validateTemplateExpression(elemID, value, type, context)
   }
 
   const referenceValidationErrors = createReferenceValidationErrors(elemID, value)
@@ -720,15 +768,15 @@ const validateValue = (
   }
 
   if (isObjectType(type) || isMapType(type)) {
-    return validateObjectValue(elemID, value, type, validatedReferences)
+    return validateObjectValue(elemID, value, type, context)
   }
 
   if (type === BuiltinTypes.UNKNOWN) {
-    return validateUnknownValue(elemID, value, validatedReferences)
+    return validateUnknownValue(elemID, value, context)
   }
 
   if (isListType(type)) {
-    return validateListValue(elemID, value, type, validatedReferences)
+    return validateListValue(elemID, value, type, context)
   }
 
   // type-wise, we can only get here if type is a primitive type
@@ -758,11 +806,11 @@ const syncGetElementAnnotationTypes = (element: TypeElement | Field): Record<str
   }
 }
 
-const validateField = (field: Field): ValidationError[] => {
+const validateField = (field: Field, context: ValidationContext): ValidationError[] => {
   const annotationTypes = syncGetElementAnnotationTypes(field)
   return Object.keys(field.annotations)
     .filter(k => annotationTypes[k] !== undefined)
-    .flatMap(k => validateValue(field.elemID.createNestedID(k), field.annotations[k], annotationTypes[k]))
+    .flatMap(k => validateValue(field.elemID.createNestedID(k), field.annotations[k], annotationTypes[k], context))
 }
 
 const validateMetaType = (element: ObjectType): ValidationError[] => {
@@ -794,14 +842,16 @@ const validateMetaType = (element: ObjectType): ValidationError[] => {
   return errors
 }
 
-const validateType = (element: TypeElement): ValidationError[] => {
+const validateType = (element: TypeElement, context: ValidationContext): ValidationError[] => {
   const annotationTypes = syncGetElementAnnotationTypes(element)
   const errors = Object.keys(element.annotations)
     .filter(k => annotationTypes[k] !== undefined)
-    .flatMap(k => validateValue(element.elemID.createNestedID('attr', k), element.annotations[k], annotationTypes[k]))
+    .flatMap(k =>
+      validateValue(element.elemID.createNestedID('attr', k), element.annotations[k], annotationTypes[k], context),
+    )
   if (isObjectType(element)) {
     const metaTypeErrors = validateMetaType(element)
-    const fieldErrors = Object.values(element.fields).flatMap(elem => validateField(elem))
+    const fieldErrors = Object.values(element.fields).flatMap(elem => validateField(elem, context))
     return errors.concat(metaTypeErrors).concat(fieldErrors)
   }
   return errors
@@ -818,15 +868,15 @@ const validateInstanceType = (elemID: ElemID, type: ObjectType): ValidationError
   return []
 }
 
-const validateInstanceElement = (element: InstanceElement): ValidationError[] => {
+const validateInstanceElement = (element: InstanceElement, context: ValidationContext): ValidationError[] => {
   const instanceType = element.refType.type
   if (!isObjectType(instanceType)) {
     // Should never happen because we resolve the element before calling this
     log.error('Found unresolved type at %s, instanceType=%o', element.elemID.getFullName(), instanceType)
     return []
   }
-  return validateValue(element.elemID, element.value, instanceType)
-    .concat(validateValue(element.elemID, element.annotations, instanceAnnotationsType))
+  return validateValue(element.elemID, element.value, instanceType, context)
+    .concat(validateValue(element.elemID, element.annotations, instanceAnnotationsType, context))
     .concat(validateInstanceType(element.elemID, instanceType))
 }
 
@@ -854,17 +904,23 @@ const validateVariableValue = (elemID: ElemID, value: Value): ValidationError[] 
 
 const validateVariable = (element: Variable): ValidationError[] => validateVariableValue(element.elemID, element.value)
 
-export const validateElement = (element: Element): ValidationError[] =>
+const createContext = (): ValidationContext => ({
+  validatedReferences: new Set<string>(),
+  requiredFieldsByType: new Map<string, Field[]>(),
+  restrictedFieldsByType: new Map<string, Field[]>(),
+})
+
+export const validateElement = (element: Element, context: ValidationContext = createContext()): ValidationError[] =>
   log.timeTrace(
     () => {
       if (isInstanceElement(element)) {
-        return validateInstanceElement(element)
+        return validateInstanceElement(element, context)
       }
       if (isVariable(element)) {
         return validateVariable(element)
       }
       if (isType(element)) {
-        return validateType(element)
+        return validateType(element, context)
       }
       return []
     },
@@ -879,9 +935,14 @@ export const validateElements = async (
   const resolved = await resolve(elements, elementsSource)
   const groupedByTopLevelId = _.groupBy(resolved, elem => elem.elemID.createTopLevelParentID().parent.getFullName())
 
+  const context = createContext()
+
   return log.timeIteratorDebug(
     wu(Object.entries(groupedByTopLevelId))
-      .map(([key, elems]) => ({ key, value: elems.flatMap(validateElement) }))
+      .map(([key, elems]) => ({
+        key,
+        value: elems.flatMap(element => validateElement(element, context)),
+      }))
       .filter(errors => errors.value.length > 0),
     'validateElements with %d elements',
     elements.length,
