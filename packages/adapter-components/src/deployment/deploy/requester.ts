5b7246bd106efc8a9528e67abb1eefc8bb822c1c
shoshana-houri <128290583+shoshana-houri@users.noreply.github.com>
2025-04-23 14:26:21 +0300

    Revert "Revert "SALTO-7771: Use the resolved change value when replacing the deploy endpoint args (#194)" (#200)" (#205)

--- packages/adapter-components/src/deployment/deploy/requester.ts
+++ packages/adapter-components/src/deployment/deploy/requester.ts
@@ -248,15 +248,14 @@ export const getRequester = <TOptions extends APIDefinitionsOptions>({
 
     const extractor = createExtractor(mergedRequestDef.transformation)
 
-    const { elemID, value } = getChangeData(change)
-
     const resolvedChange = await changeResolver(change)
+    const resolvedValue = getChangeData(resolvedChange).value
     const contextFunc =
       mergedRequestDef.context?.custom !== undefined
         ? mergedRequestDef.context.custom(mergedRequestDef.context)
         : () => undefined
     const additionalContext = replaceAllArgs({
-      context: _.merge({}, getChangeData(resolvedChange).value, getChangeData(resolvedChange).annotations),
+      context: _.merge({}, resolvedValue, getChangeData(resolvedChange).annotations),
       value: _.merge(
         contextFunc({ change, ...changeContext }),
         _.omit(mergedRequestDef.context, ['change', 'changeGroup', 'elementSource', 'sharedContext', 'custom']),
@@ -269,7 +268,7 @@ export const getRequester = <TOptions extends APIDefinitionsOptions>({
           change,
           ...changeContext,
           additionalContext,
-          value: recursiveNaclCase(getChangeData(resolvedChange).value, true),
+          value: recursiveNaclCase(resolvedValue, true),
         })
 
     throwOnUnresolvedReferences(data)
@@ -279,7 +278,7 @@ export const getRequester = <TOptions extends APIDefinitionsOptions>({
         mergedEndpointDef.queryArgs !== undefined
           ? replaceAllArgs({
               value: mergedEndpointDef.queryArgs,
-              context: _.merge({}, value, additionalContext),
+              context: _.merge({}, resolvedValue, additionalContext),
             })
           : undefined,
       headers: mergedEndpointDef.headers,
@@ -288,7 +287,7 @@ export const getRequester = <TOptions extends APIDefinitionsOptions>({
 
     log.trace(
       'making request for change %s client %s endpoint %s.%s',
-      elemID.getFullName(),
+      getChangeData(change).elemID.getFullName(),
       clientName,
       mergedRequestDef.endpoint.path,
       mergedRequestDef.endpoint.method,
@@ -296,7 +295,7 @@ export const getRequester = <TOptions extends APIDefinitionsOptions>({
 
     const finalEndpointIdentifier = replaceAllArgs({
       value: mergedEndpointDef,
-      context: _.merge({}, value, additionalContext),
+      context: _.merge({}, resolvedValue, additionalContext),
       throwOnUnresolvedArgs: true,
     })
     const client = clientDefs[clientName].httpClient
